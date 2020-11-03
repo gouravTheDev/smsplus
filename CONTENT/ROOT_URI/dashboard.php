196509d9d9eda154f99868e74744536c21003e9b
Gourav Chatterjee <chatterjeegouravking@gmail.com>
2020-11-03 11:31:04 +0530

    Changes

--- CONTENT/ROOT_URI/dashboard.php
+++ CONTENT/ROOT_URI/dashboard.php
@@ -1,4 +1,10 @@
 <?php include '_menu.php'; ?>
+<?php 
+	if (!$_SESSION['LoggedIn']) {
+		echo "<script>window.location.href='login';</script>";
+	}else{
+?>
+
 <div class="content" style="background: black; color: #fff;">
 	<main id="main">
 		<div class="container" style="padding-top: 10%;">
@@ -7,3 +13,6 @@
 		</div>
 	</main>
 </div>
+<?php 
+	}
+ ?>
