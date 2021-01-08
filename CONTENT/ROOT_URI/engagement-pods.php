39fc0ddd9ceeb80ead9f45632f6c3f7a59c29d5b
gourav <chatterjeegouravking@gmail.com>
2021-01-08 10:01:13 +0530

    ent icon

--- CONTENT/ROOT_URI/engagement-pods.php
+++ CONTENT/ROOT_URI/engagement-pods.php
@@ -239,7 +239,7 @@
                 <a href="https://t.me/SMSEntrpreneurs" style="color: #fff; text-decoration: none;">
                   <div class="card menus mx-auto" style="">
                     <div class="card-body text-center">
-                      <img src="assets/engagement-pods/ent.png" height="100%;">
+                      <img src="assets/engagement-pods/ent.png" style="height: 100%; width: auto;">
                     </div>
                   </div>
                   <h5 class="card-title iconText mt-3">Entrepreneurs</h5>
