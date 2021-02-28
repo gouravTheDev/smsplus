f649c50a42f6e100ce1aacde8a6d21f48ca12602
gourav <chatterjeegouravking@gmail.com>
2021-02-28 22:30:59 +0530

    Changes

--- CONTENT/ROOT_URI/admin/service-type/api/index.php
+++ CONTENT/ROOT_URI/admin/service-type/api/index.php
@@ -1,35 +1,40 @@
-<?php 
+<?php
 $link = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
 $link->set_charset("utf8mb4");
 
 if ($_SESSION['AdminLoggedIn']) {
 
 	if (isset($_GET['getData'])) {
-	 $typeId = $_GET['typeId'];
-	 $sql = "SELECT * FROM SERVICE_TYPE WHERE `TYPE_ID` = '$typeId' AND DELETED = 'FALSE'";
-	 $result = mysqli_query($link,$sql);
-	 if ($result) {
-	    if(mysqli_num_rows($result)>0){
-	      $data = mysqli_fetch_array($result,MYSQLI_ASSOC);
-	    }else{
-	      $data = null;
-	    }
-	    $data = json_encode($data);
-	  }
+		$typeId = $_GET['typeId'];
+		$sql = "SELECT * FROM SERVICE_TYPE WHERE `TYPE_ID` = '$typeId' AND DELETED = 'FALSE'";
+		$result = mysqli_query($link, $sql);
+		if ($result) {
+			if (mysqli_num_rows($result) > 0) {
+				$data = mysqli_fetch_array($result, MYSQLI_ASSOC);
+			} else {
+				$data = null;
+			}
+			$data = json_encode($data);
+		}
 	}
 
 	if (isset($_GET['deleteServiceType'])) {
-	 $typeId = $_GET['typeId'];
-	 $sql = "UPDATE SERVICE_TYPE SET DELETED = 'TRUE' WHERE TYPE_ID = '$typeId'";
-	 $result = mysqli_query($link,$sql);
-	 if ($result) {
-	    $data = json_encode("Deleted Successfully");
-	  }else{
-	    $data = null;
-	  }
+		$typeId = $_GET['typeId'];
+		$sqlS = "SELECT * FROM SERVICE WHERE TYPE_ID = '$typeId' AND DELETED = 'FALSE'";
+		$resultS = mysqli_query($link, $sqlS);
 
+		if (mysqli_num_rows($resultS) > 0) {
+			$data = json_encode("Could Not Delete Category");
+		} else {
+			$sql = "UPDATE SERVICE_TYPE SET DELETED = 'TRUE' WHERE TYPE_ID = '$typeId'";
+			$result = mysqli_query($link, $sql);
+			if ($result) {
+				$data = json_encode("Deleted Successfully");
+			} else {
+				$data = null;
+			}
+		}
 	}
 
 	echo $data;
 }
-?>
