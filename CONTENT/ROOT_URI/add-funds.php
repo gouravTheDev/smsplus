6b87e58c8d8641151823e524f341ac9ca3a58c8b
gourav <chatterjeegouravking@gmail.com>
2021-03-29 23:24:17 +0530

    Paypal Done

--- CONTENT/ROOT_URI/add-funds.php
+++ CONTENT/ROOT_URI/add-funds.php
@@ -1,52 +1,203 @@
+<script>
+  if ( window.history.replaceState ) {
+    window.history.replaceState( null, null, window.location.href );
+  }
+</script>
 <title>SocialMySocial+ Add Funds</title>
 
 <?php include '_menuL.php'; ?>
-<?php 
-	if (!$_SESSION['LoggedIn']) {
-		echo "<script>window.location.href='login';</script>";
-	}else{
+
+<script
+    src="https://www.paypal.com/sdk/js?client-id=<?php echo PAYPAL_CLIENT_ID; ?>">
+</script>
+<?php
+if (!$_SESSION['LoggedIn']) {
+  echo "<script>window.location.href='login';</script>";
+} else {
 ?>
 
-<div class="page-breadcrumb bg-white">
+  <div class="page-breadcrumb bg-white">
     <div class="row align-items-center">
-        <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 pt-3">
-            <h4 class="page-title text-uppercase font-medium font-14">Add Funds</h4>
+      <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12 pt-3">
+        <h4 class="page-title text-uppercase font-medium font-14">Add Funds</h4>
+      </div>
+      <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+        <div class="d-md-flex">
+          <ol class="breadcrumb ml-auto">
+            <!-- <li><a href="#">Funds</a></li> -->
+          </ol>
         </div>
-        <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
-            <div class="d-md-flex">
-                <ol class="breadcrumb ml-auto">
-                    <!-- <li><a href="#">Funds</a></li> -->
-                </ol>
-            </div>
-        </div>
+      </div>
     </div>
+  </div>
+
+  <div class="container-fluid pt-3 col-md-8 col-sm-12 mx-auto">
+    <div class="card card-tabs">
+      <div class="card-header p-0 pt-1">
+        <ul class="nav nav-tabs" id="custom-tabs-one-tab" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="custom-tabs-one-active-tab" data-toggle="pill" href="#paypal-payment" role="tab" aria-controls="custom-tabs-one-profile" aria-selected="false">Paypal Payment</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="custom-tabs-one-archive-tab" data-toggle="pill" href="#stripe-payment" role="tab" aria-controls="custom-tabs-one-profile" aria-selected="false">Stripe Payment</a>
+          </li>
+        </ul>
+      </div>
+      <div class="card-body">
+        <div class="tab-content" id="custom-tabs-one-tabContent">
+         
+          <!-- Paypal tab Starts -->
+
+          <div class="tab-pane fade show active" id="paypal-payment" role="tabpanel" aria-labelledby="custom-tabs-one-profile-tab">
+            <div class="text-center mb-3">
+                <img src="/assets/images/paypal.png" style="height: 80px; width: 280px;">
+                <p>You can deposit funds with Paypal®. They will be automaticly added into your account!</p>
+            </div>
+            <div class="row">
+                <div class="col-md-11 col-sm-12 mx-auto">
+                    <div class="form-group">
+                        <label>Amount (USD)</label>
+                        <input type="number" name="amount" id="amount" class="form-control" placeholder="10">
+                    </div>
+                </div>
+                <?php $userId = $_SESSION["userId"]; ?>
+                <input type="hidden" id="userId" value="<?php echo $userId; ?>">
+                <div class="col-md-11 col-sm-12 mx-auto">
+                    <h5>Note:</h5>
+                    <ul>
+                        <li>Minimum Payment: $10</li>
+                        <li>Maximum Payment: $10000000</li>
+                        <li>Your Payment Credentials are safe with us.</li>
+                    </ul>
+                </div>
+                <div class="col-md-8 col-sm-12 mx-auto text-center mt-4">
+                    <div id="paypal-button-container"></div>
+                </div>
+                <div class="col-12 mx-auto">
+                    <div class="msgs">
+                       <div class="alert alert-warning my-2 text-center font-weight-bold" id="paymentWarningPaypal" style="display: none;">Please wait. Don't refresh the page. We are processing your payment.</div>
+                       <div class="alert alert-warning my-2 text-center font-weight-bold" id="paymentWarningPaypal2" style="display: none;"></div>
+                       <div class="alert alert-danger my-2 text-center font-weight-bold" id="paymentFailedPaypal" style="display: none;">Payment could not be completed. Please try again or contact support!</div>
+                    </div>
+                </div>
+            </div>
+          </div>
+
+          <!-- Paypal tab Starts -->
+
+          <!-- Stripe tab Starts -->
+
+          <div class="tab-pane fade" id="stripe-payment" role="tabpanel" aria-labelledby="custom-tabs-one-profile-tab">
+            <div class="text-center mb-3">
+                <img src="/assets/images/paypal.png" style="height: 100px;">
+                <p>You can deposit funds with Paypal®. They will be automaticly added into your account!</p>
+            </div>
+
+          </div>
+
+          <!-- Stripe tab Ends -->
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter" style="display: none; " id="SuccessmodalButton">
+  Launch demo modal
+</button>
+
+<!-- Success Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <div class="row my-4 py-4">
+            <div class="col-12 text-center">
+                <img src="/assets/images/checked.svg" style="height: 150px; width: 150px;">
+                <h3 class="font-weight-bold mt-3">Thank You.</h3>
+                <h3 class="font-weight-bold mt-3">Your Transaction is successful.</h3>
+            </div>  
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
-<div class="container-fluid pt-3">
-    <!-- <h1 class="text-center">Add Funds</h1> -->
-    <div class="col-md-8 col-sm-12 mx-auto">
-    	<div class="card shadow">
-    		<div class="card-body" style="font-size: 1.2em;">
-    			<form method="POST" class="col-md-10 col-sm-12 mx-auto">
-    				<div class="form-group">
-    					<label>Method</label>
-    					<select class="form-control" name="method">
-    						<option value="paypal">PayPal / Credit Card (Min. $20)</option>
-    						<option value="visa">Visa / Master Card - 5% Bonus</option>
-    					</select>
-    				</div>
-    				<div class="form-group">
-    					<label>Amount</label>
-						<input type="number" name="amount" class="form-control" placeholder="Enter Amount">
-    				</div>
-    				<div class="form-group mt-4 pt-3">
-    					<button type="button" class="btn btn-block btnSmm">Proceed to Pay</button>
-    				</div>
-    			</form>
-    		</div>
-    	</div>
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter2" style="display: none; " id="ErrormodalButton">
+  Launch demo modal2
+</button>
+
+<!-- Success Modal -->
+<div class="modal fade" id="exampleModalCenter2" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <div class="row my-4 py-4">
+            <div class="col-12 text-center">
+                <img src="/assets/images/cancel.svg" style="height: 150px; width: 150px;">
+                <h3 class="font-weight-bold mt-3">We are sorry.</h3>
+                <h3 class="font-weight-bold mt-3">Your Transaction could not be completed. Please try again!</h3>
+            </div>  
+        </div>
+      </div>
     </div>
+  </div>
 </div>
-<?php 
-	}
+ 
+  <script>
+    paypal.Buttons({
+        createOrder: function(data, actions) {
+          document.getElementById('paymentWarningPaypal').style.display = "block";
+          // This function sets up the details of the transaction, including the amount and line item details.
+          var priceAmount = document.getElementById('amount').value;
+          if (!priceAmount || priceAmount == '' || parseInt(priceAmount)<10) {
+            document.getElementById('paymentWarningPaypal').style.display = "none";
+            document.getElementById('paymentWarningPaypal2').style.display = "block";
+            document.getElementById('paymentWarningPaypal2').innerHTML =  "Minimum Amount is $10";
+            return;
+          }else{
+            document.getElementById('paymentWarningPaypal2').style.display = "none";
+            return actions.order.create({
+                purchase_units: [{
+                  amount: {
+                    "value": priceAmount
+                  }
+                }]
+              });
+          }
+        },
+        onApprove: function(data) {
+            document.getElementById('paymentWarningPaypal2').style.display = "none";
+            var userId = document.getElementById('userId').value;
+            let formData = new FormData();
+            formData.append('orderID', data.orderID);
+            formData.append('userId', userId);
+            return fetch('/paypalPaymentHandle.php', {
+              method: "POST",
+              body: formData
+            }).then(function(res) {
+              return res.json();
+            }).then(function(details) {
+                document.getElementById('paymentWarningPaypal').style.display = "none";
+                if (details.result.status == 'COMPLETED') {
+                    document.getElementById('amount').value = '';
+                    document.getElementById('SuccessmodalButton').click();
+                }
+            });
+          },
+        onCancel: function (data) {
+            document.getElementById('paymentWarningPaypal2').style.display = "none";
+            document.getElementById('paymentWarningPaypal').style.display = "none";
+           document.getElementById('ErrormodalButton').click();
+        }
+      }).render('#paypal-button-container');
+</script>
+<?php
+}
 ?>
---
+++ paypalPaymentHandle.php
@@ -0,0 +1,134 @@
+<?php
+
+// Capture Payment
+
+namespace Sample\CaptureIntentExamples;
+require __DIR__ . '/vendor/autoload.php';
+include 'paypalClient.php';
+
+use Sample\PayPalClient;
+use PayPalCheckoutSdk\Orders\OrdersCaptureRequest;
+// use PayPalCheckoutSdk\Orders\OrdersGetRequest;
+
+class CaptureOrder
+{
+  public static function captureOrder($orderId, $debug=false, $userId)
+  {
+
+    $link = new \MySQLi(MYSQL_HOST,MYSQL_USER,MYSQL_PASS,MYSQL_DB);
+    $link->set_charset("utf8");
+
+    $sqlF = "SELECT * FROM USERS WHERE USER_ID = '$userId'";
+    $resultF = mysqli_query($link, $sqlF);
+
+    $rowF = mysqli_fetch_array($resultF, MYSQLI_ASSOC);
+    $wallet = $rowF['WALLET_MONEY'];
+    $emailId = $rowF['EMAIL'];
+    $fullName = $rowF['FIRST_NAME']. " ".$rowF['LAST_NAME'];
+
+    $request = new OrdersCaptureRequest($orderId);
+
+    // 3. Call PayPal to capture an authorization
+    $client = PayPalClient::client();
+    $response = $client->execute($request);
+
+    if ($response) {
+
+      $todayDate = date("Y-m-d");
+
+      $orderId = $response->result->id;
+      $status = $response->result->status;
+      $paymentId = $response->result->purchase_units[0]->payments->captures[0]->id;
+      $currency = $response->result->purchase_units[0]->payments->captures[0]->amount->currency_code;
+      $amountValue = $response->result->purchase_units[0]->payments->captures[0]->amount->value;
+      $payerName = $response->result->purchase_units[0]->shipping->name->full_name;
+
+      $sql = "INSERT INTO PAYMENTS (`ORDER_ID`, `PAYMENT_ID`, `USER_ID`, `AMOUNT`, `CURRENCY`, `PAYMENT_TYPE`, `DATE`, `STATUS`)VALUES('$orderId', '$paymentId', '$userId', '$amountValue', '$currency', 'Paypal', '$todayDate', '$status')";
+      $result = mysqli_query($link, $sql);
+
+      if ($result) {
+
+        if (!$wallet || $wallet == '') {
+          $wallet = $amountValue;
+        }else{
+          $wallet = (int) $wallet;
+          $wallet = $wallet+$amountValue;
+        }
+
+        //Update User Account 
+        $sqlU = "UPDATE USERS SET `WALLET_MONEY` = '$wallet' WHERE USER_ID = '$userId'";
+        $resultU = mysqli_query($link, $sqlU);
+
+        // //Send notification to admin
+        // $dateTime = date('Y-m-d H:i:s');
+        // $title = "New Payment received";
+        // $message = "Payment has been done for assignment- ".$assignmentId;
+        // $linkToGo = "assignmentdetails?assignmentId=".$assignmentId;
+
+        // $stmtNo = $link->prepare("INSERT INTO NOTIFICATIONS (`USER_TYPE`, `USER_ID`, `TITLE`, `MESSAGE`, `LINK`, `DATE_TIME`, `STATUS`)VALUES('ADMIN', ?, ?, ?, ?, ?, 'pending')");
+
+        // $stmtNo->bind_param("sssss", $userId, $title, $message, $linkToGo, $dateTime);
+
+        // $resultInsertNo = $stmtNo->execute();
+
+        // SEND MAIL TO USER
+        // $mailSubject = "Balance Added to Wallet";
+        // $mailMessage = " <html>
+        //           <h3>Welcome ". $fullName." to Best Grade Assignment. We have the best professional experts and we provide the best solution to every assignment. We provide the best assignment solution at a cheap rate and fast. </h3>
+        //           <h3>You have paid for assignment with assignment Id:- ".$assignmentId."</h3>
+        //           <h3>Payment Details:- </h3>
+        //           <h3>Amount:- ".$currency."-".$amountValue."</h3>
+        //           <h3>Payer Name:- ".$payerName."</h3>
+        //           <h3>Your payment will be processed and approved by our experts. For any update regarding your assignment you will be notified in your dashboard. </h3>
+        //           <h3>You can manage your assignments in your <a href='https://web.bestgradeassignment.com/'>dashboard </a> anytime.</h3>
+        //           <p>You can check our <a href='https://bestgradeassignment.com/'>website </a>for more information. </p>
+        //           <p>Thanks and regards.</p>
+        //           </body>
+        //           </html>";
+        // $headers = "MIME-Version: 1.0" . "\r\n";
+        // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // $headers .= 'From: Best Grade<no-reply@bestgradeassignment.com>' . "\r\n";
+
+        // mail($emailId = $rowF['EMAIL'];,$mailSubject,$mailMessage,$headers);
+
+        // //Send mail to admin
+        // $mailSubject = "Payment Received for assignment Id:- ".$assignmentId;
+        // $mailMessage = " <html>
+        //           <h3>Welcome to Best Grade Assignment Admin Panel. </h3>
+        //           <h3>A new payment has been received for an assignment with Assignment Id:- ".$assignmentId.". </h3>
+        //           <h3>Payment Id:- ".$captureId."</h3>
+        //           <h3>User Id:- ".$userId."</h3>
+        //           <h3>Amount:- ".$currency."-".$amountValue."</h3>
+        //           <h3>Payer Name:- ".$payerName."</h3>
+        //           <h3>Please review the payment and approve the assignment.  </h3>
+        //           <h3>You can manage the assignments in your <a href='https://web.bestgradeassignment.com/admin/'>dashboard </a> anytime.</h3>
+        //           <p>Thanks and regards.</p>
+        //           </body>
+        //           </html>";
+        // $headers = "MIME-Version: 1.0" . "\r\n";
+        // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // $headers .= 'From: Best Grade<no-reply@bestgradeassignment.com>' . "\r\n";
+
+        // mail(ADMIN_MAIL,$mailSubject,$mailMessage,$headers);
+
+
+        echo json_encode($response);
+      }else{
+        echo json_encode(mysqli_error($link));
+      }
+    }
+
+    // echo json_encode($response);
+  }
+}
+
+if (isset($_POST['orderID'])) {
+  $orderId = $_POST['orderID'];
+  $userId = $_POST['userId'];
+  CaptureOrder::captureOrder($orderId, true, $userId);
+}
+
+
+?>
