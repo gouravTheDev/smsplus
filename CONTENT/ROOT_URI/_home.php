e243d98b02216d4120105a70c1941f7b29a61229
Gourav Chatterjee <39953697+gouravTheDev@users.noreply.github.com>
2021-07-29 20:32:04 +0530

    Update _home.php

--- CONTENT/ROOT_URI/_home.php
+++ CONTENT/ROOT_URI/_home.php
@@ -4,62 +4,60 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Padauk&display=swap" rel="stylesheet">
   <title>Coming Soon!</title>
-  <style>
-  body, html {
-    height: 100%;
-    margin: 0;
-    font-family: 'Padauk', sans-serif;
-  }
-  .bgimg {
-    /*background-image: url('/assets/coming.jpg');*/
-    background: #000704;
-    height: 100%;
-    background-position: center;
-    background-size: cover;
-    position: relative;
-    color: white;
-    font-family: 'Padauk', sans-serif;
-    font-size: 25px;
-  }
-  .topleft {
-    position: absolute;
-    top: 0;
-    left: 16px;
-  }
-  .bottomleft {
-    position: absolute;
-    bottom: 0;
-    left: 16px;
-  }
-  .middle {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    text-align: center;
-  }
-  hr {
-    margin: auto;
-    width: 40%;
-  }
-  @media only screen and (max-width: 600px) {
-    h1{
-      font-size: 1.1em;
-    }
-  }
-</style>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300&display=swap" rel="stylesheet">
 </head>
-
+<style>
+body, html {
+  height: 100%;
+  margin: 0;
+  font-family: 'Heebo', sans-serif;
+}
+.bgimg {
+  /*background-image: url('/assets/coming.jpg');*/
+  background: #000704;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+  color: white;
+  font-size: 25px;
+}
+.topleft {
+  position: absolute;
+  top: 0;
+  left: 16px;
+}
+.bottomleft {
+  position: absolute;
+  bottom: 0;
+  left: 16px;
+}
+.middle {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+hr {
+  margin: auto;
+  width: 40%;
+}
+@media only screen and (max-width: 600px) {
+  h1{
+    font-size: 0.8em;
+  }
+}
+</style>
 <body>
 
 <div class="bgimg">
   <div class="middle">
-    <img src="/LOGOS/logo.png" style="height: 80px; margin-top: 15px; margin-bottom: 0px;">
-    <h1>COMING SOON</h1>
+    <img src="logo.png" style="height: 80px; margin-top: 15px; margin-bottom: 0px;">
+    <h1>C O M I N G &nbsp; S O O N</h1>
     <hr>
   </div>
   <div class="bottomleft">
