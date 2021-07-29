4f3ccb41ad47d9637dc6b51cb860606ff24069c1
Gourav Chatterjee <39953697+gouravTheDev@users.noreply.github.com>
2021-07-29 18:37:54 +0530

    Update _home.php

--- CONTENT/ROOT_URI/_home.php
+++ CONTENT/ROOT_URI/_home.php
@@ -5,50 +5,51 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <title>Coming Soon!</title>
-</head>
-<style>
-body, html {
-  height: 100%;
-  margin: 0;
-}
-.bgimg {
-  /*background-image: url('/assets/coming.jpg');*/
-  background: #000704;
-  height: 100%;
-  background-position: center;
-  background-size: cover;
-  position: relative;
-  color: white;
-  font-family: "Courier New", Courier, monospace;
-  font-size: 25px;
-}
-.topleft {
-  position: absolute;
-  top: 0;
-  left: 16px;
-}
-.bottomleft {
-  position: absolute;
-  bottom: 0;
-  left: 16px;
-}
-.middle {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  text-align: center;
-}
-hr {
-  margin: auto;
-  width: 40%;
-}
-@media only screen and (max-width: 600px) {
-  h1{
-    font-size: 1.1em;
+  <style>
+  body, html {
+    height: 100%;
+    margin: 0;
   }
-}
+  .bgimg {
+    /*background-image: url('/assets/coming.jpg');*/
+    background: #000704;
+    height: 100%;
+    background-position: center;
+    background-size: cover;
+    position: relative;
+    color: white;
+    font-family: "Courier New", Courier, monospace;
+    font-size: 25px;
+  }
+  .topleft {
+    position: absolute;
+    top: 0;
+    left: 16px;
+  }
+  .bottomleft {
+    position: absolute;
+    bottom: 0;
+    left: 16px;
+  }
+  .middle {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+  }
+  hr {
+    margin: auto;
+    width: 40%;
+  }
+  @media only screen and (max-width: 600px) {
+    h1{
+      font-size: 1.1em;
+    }
+  }
 </style>
+</head>
+
 <body>
 
 <div class="bgimg">
