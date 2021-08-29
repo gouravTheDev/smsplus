86b4a0fe2292839ee53689bb93e5cc53652614c2
Gourav Chatterjee <39953697+gouravTheDev@users.noreply.github.com>
2021-08-29 22:29:20 +0530

    Update index.php

--- CONTENT/ROOT_URI/index.php
+++ CONTENT/ROOT_URI/index.php
@@ -1,25 +1,25 @@
 <?php 
 $lnk2 = explode('?', $lnk);
-// print_r($lnk2);
-// if ($_SESSION['LoggedIn']) {
-// 	include("_headerL.php");
-// 	if($lnk == "" || $lnk == "index") include("dashboard.php");
-//     elseif(file_exists(__DIR__."/".$lnk.".php"))  include($lnk.".php");
-//     elseif(isset($lnk2[1]) && file_exists(__DIR__."/".$lnk2[0].".php") ) include($lnk2[0].".php");
-//     else include '_404.php';
-// 	include("_footerL.php");
-// }else{
-// 	if($lnk=="login") include("login.php");
-// 	elseif($lnk=="signup") include("signup.php");
-// 	elseif($lnk=="forgotpassword") include("forgotpassword.php");
-// 	else{
-// 		include("_header.php");
-// 		if($lnk=="" ||$lnk == "index") include("_home.php");
-// 	    elseif(file_exists(__DIR__."/".$lnk.".php"))  include($lnk.".php");
-// 	    elseif(isset($lnk2[1]) && file_exists(__DIR__."/".$lnk2[0].".php") ) include($lnk2[0].".php");
-// 	    else include '_404.php';
-// 		include("_footer.php");
-// 	}	
-// }
-if($lnk == "" || $lnk == "index") include("_home.php");
+print_r($lnk2);
+if ($_SESSION['LoggedIn']) {
+	include("_headerL.php");
+	if($lnk == "" || $lnk == "index") include("dashboard.php");
+    elseif(file_exists(__DIR__."/".$lnk.".php"))  include($lnk.".php");
+    elseif(isset($lnk2[1]) && file_exists(__DIR__."/".$lnk2[0].".php") ) include($lnk2[0].".php");
+    else include '_404.php';
+	include("_footerL.php");
+}else{
+	if($lnk=="login") include("login.php");
+	elseif($lnk=="signup") include("signup.php");
+	elseif($lnk=="forgotpassword") include("forgotpassword.php");
+	else{
+		include("_header.php");
+		if($lnk=="" ||$lnk == "index") include("_home.php");
+	    elseif(file_exists(__DIR__."/".$lnk.".php"))  include($lnk.".php");
+	    elseif(isset($lnk2[1]) && file_exists(__DIR__."/".$lnk2[0].".php") ) include($lnk2[0].".php");
+	    else include '_404.php';
+		include("_footer.php");
+	}	
+}
+// if($lnk == "" || $lnk == "index") include("_home.php");
 ?>
